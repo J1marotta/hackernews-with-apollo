cacef4ccb359996ac789015a43617bc26188b19b
James Marotta <james.marotta@outlook.com>
2019-08-21 17:36:38 +1000

    First successfull test run

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/App.css'
+import LinkList from './LinkLlist'
+
+function App() {
+  return <LinkList />
+}
+
+export default App
---
+++ src/components/Link.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Link = ({ description, url }) => (
+  <div>
+    <div>
+      {description} {url}
+    </div>
+  </div>
+)
+
+export default Link
